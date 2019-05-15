2676d9aa47beb8377ed599982f098daaba33c1f0
Abhinav Goyal <ag@abhinavgoyal.info>
2019-05-16 08:12:49 +1000

    Step2: Added headers to the files
    The csvs are now being generated with headers so we know the column
    names while importing into a neo4j DB.

--- migrate.sql
+++ migrate.sql
@@ -1,4 +1,23 @@
-select * into outfile '/var/lib/mysql-files/employeelist.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from employeelist;
-select * into outfile '/var/lib/mysql-files/message.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from message;
-select * into outfile '/var/lib/mysql-files/recipientinfo.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from recipientinfo;
-select * into outfile '/var/lib/mysql-files/referenceinfo.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from referenceinfo;
+select 'eid', 'firstName', 'lastName', 'Email_id', 'Email2', 'Email3', 'EMail4', 'folder', 'status'
+union all
+select eid, firstName, lastName, Email_id, Email2, Email3, EMail4, folder, status
+from employeelist
+into outfile '/var/lib/mysql-files/employeelist.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n';
+
+select 'mid', 'sender', 'date', 'message_id', 'subject', 'folder'
+union all
+select mid, sender, date, message_id, subject, folder
+from message
+into outfile '/var/lib/mysql-files/message.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n';
+
+select 'rid', 'mid', 'rtype', 'rvalue', 'dater'
+union all
+select rid, mid, rtype, rvalue, dater
+from recipientinfo
+into outfile '/var/lib/mysql-files/recipientinfo.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n';
+
+select 'rfid', 'mid', 'reference'
+union all
+select rfid, mid, reference
+from referenceinfo
+into outfile '/var/lib/mysql-files/referenceinfo.csv'  fields terminated by ',' optionally enclosed by '"' lines terminated by '\n';
